73f37cadc2f3855e1f68566ba6eed8f8e40e2f68
Jaroslav Sykora <jaroslav.sykora@gmail.com>
2023-05-15 21:02:33 +0200

    smc: implement ps2k cmd send and status report
    problem: bus controller does not update rambank_nr on CPU write!!!
    after unrelated change in the smc!

--- fpga-nora/src/smc.v
+++ fpga-nora/src/smc.v
@@ -1,6 +1,12 @@
 /**
- * System Management Controller defined by CX16
- * 
+ * System Management Controller defined by CX16.
+ * It primarily responds on I2C bus as the slave device at address 0x42.
+ * It provides these registers over I2C:
+ *      0x07		Read from keyboard buffer
+ *      0x18		Read ps2 (keyboard) status
+ *      0x19	    00..FF	Send ps2 command
+ *      0x21		Read from mouse buffer     <- TBD, not implemented yet here!!
+ *
  */
 module smc (
     // Global signals
@@ -23,6 +29,13 @@ module smc (
     parameter SMCREG_READ_KBD_BUF = 8'h07;
     parameter SMCREG_READ_PS2_KBD_STAT = 8'h18;
     parameter SMCREG_SEND_PS2_KBD_CMD = 8'h19;
+
+    
+    // PS2_CMD_STATUS : uint8_t
+    parameter PS2_CMD_STAT_IDLE = 8'h00;
+    parameter PS2_CMD_STAT_PENDING = 8'h01;
+    parameter PS2_CMD_STAT_ACK = 8'hFA;
+    parameter PS2_CMD_STAT_ERR = 8'hFE;
 
     reg [7:0]   smc_regnum;         // SMC register address
 
@@ -66,8 +79,15 @@ module smc (
         .ck1us (ck1us)
     );
 
-    wire [7:0]  ps2k_code;
-    wire        ps2k_codevalid;
+    wire [7:0]  ps2k_rxcode;
+    wire        ps2k_rxcodevalid;
+
+    reg [7:0]   ps2k_txcode;
+    reg         ps2k_txcodevalid;
+
+    wire  ps2k_busy;
+    wire  ps2k_acked;
+    wire  ps2k_errd;
 
 
     // PS2 Keyboard Port
@@ -83,22 +103,22 @@ module smc (
         .PS2_CLKDR0 (PS2K_CLKDR),     // 1=>drive zero on CLK, 0=>HiZ
         .PS2_DATADR0 (PS2K_DATADR),    // 1=>drive zero on DATA, 0=>HiZ
         // 
-        .code_rx_o (ps2k_code),       // received scan-code
-        .code_rx_v_o (ps2k_codevalid),       // scan-code valid
+        .code_rx_o (ps2k_rxcode),       // received scan-code
+        .code_rx_v_o (ps2k_rxcodevalid),       // scan-code valid
         // Host-to-Device (TX) interface
-        .cmd_tx_i (8'h00),         // command byte to send   
-        .cmd_tx_v_i (1'b0),         // send the command byte; recognized only when busy=0
-        .busy (),             // ongoing RX/TX (not accepting a new command now)
-        .tx_acked_o (),         // our TX commend byte was ACKed by device
-        .tx_errd_o ()           // we got a NACK at the end of command sending
+        .cmd_tx_i (ps2k_txcode),         // command byte to send   
+        .cmd_tx_v_i (ps2k_txcodevalid),         // send the command byte; recognized only when busy=0
+        .busy (ps2k_busy),             // ongoing RX/TX (not accepting a new command now)
+        .tx_acked_o (ps2k_acked),         // our TX commend byte was ACKed by device
+        .tx_errd_o (ps2k_errd)           // we got a NACK at the end of command sending
     );
 
     // HACK
     // assign nora_slv_datard = (nora_slv_req_SCRB) ? nora_slv_addr[7:0] : via1_slv_datard;
-    // assign nora_slv_datard = (nora_slv_req_SCRB) ? ps2k_code : via1_slv_datard;
+    // assign nora_slv_datard = (nora_slv_req_SCRB) ? ps2k_rxcode : via1_slv_datard;
 
     wire kbdfifo_full;
-    wire ps2k_enq = ps2k_codevalid & !kbdfifo_full;
+    wire ps2k_enq = ps2k_rxcodevalid & !kbdfifo_full;
     wire kbdfifo_empty;
     reg kbdfifo_deq;
     wire [7:0] kbdfifo_rdata;
@@ -112,7 +132,7 @@ module smc (
         .clk6x (clk6x),      // 48MHz
         .resetn (resetn),     // sync reset
         // I/O Write port
-        .wport_i (ps2k_code),          // Write Port Data
+        .wport_i (ps2k_rxcode),          // Write Port Data
         .wenq_i (ps2k_enq),                 // Enqueue data from the write port now; must not assert when full_o=1
         // I/O read port
         .rport_o (kbdfifo_rdata),          // Read port data: valid any time empty_o=0
@@ -123,6 +143,8 @@ module smc (
     );    
 
 
+    reg [7:0]  kbd_stat;
+
     always @(posedge clk6x) 
     begin
         if (!resetn)
@@ -131,9 +153,26 @@ module smc (
             byteidx <= 2'b00;
             smc_regnum <= 8'h00;
             kbdfifo_deq <= 0;
+            ps2k_txcode <= 8'h00;
+            ps2k_txcodevalid <= 0;
+            kbd_stat <= PS2_CMD_STAT_IDLE;
         end else begin
             // clear one-off signals
             kbdfifo_deq <= 0;
+
+            if (!busy)
+            begin
+                ps2k_txcodevalid <= 0;
+            end
+
+            if (ps2k_acked)
+            begin
+                kbd_stat <= PS2_CMD_STAT_ACK;
+            end else if (ps2k_errd)
+            begin
+                kbd_stat <= PS2_CMD_STAT_ERR;
+            end
+
 
             if (devsel)
             begin
@@ -145,6 +184,15 @@ module smc (
                     begin
                         // it is the first i2c data byte after address: the register number
                         smc_regnum <= rxbyte;
+                    end else if (byteidx == 2'b01)
+                    begin
+                        case (smc_regnum)
+                            SMCREG_SEND_PS2_KBD_CMD:
+                            begin
+                                ps2k_txcode <= rxbyte;
+                                ps2k_txcodevalid <= 1;
+                            end
+                        endcase
                     end
                     
                     byteidx <= byteidx + 1;
@@ -166,7 +214,7 @@ module smc (
 
                         SMCREG_READ_PS2_KBD_STAT:
                         begin
-                            txbyte <= 8'hFA;
+                            txbyte <= kbd_stat;
                         end
                     endcase
 
