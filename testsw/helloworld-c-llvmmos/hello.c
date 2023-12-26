708ff4b046a5523095b780b88439204a7547892c
Jaroslav Sykora <jaroslav.sykora@gmail.com>
2023-12-26 22:13:42 +0100

    add helloworld in C llvm-mos

---
+++ testsw/helloworld-c-llvmmos/hello.c
@@ -0,0 +1,12 @@
+/**
+ * Minimal HELLO WORLD.
+ * Compiler: llvm-mos, generates .prg file
+ * Can be loaded through ICD do-loadprg.py
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("HELLO WORLD FROM C!\r\n");
+    return 0;
+}
